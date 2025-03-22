ca1e86c4a4cab900dacf37ccaa552fc377f48a58
Yash Yadav <yashyadav.sr@gmail.com>
2025-03-22 17:36:56 +0530

    feat: added shorts selection

--- src/content-scripts/main.js
+++ src/content-scripts/main.js
@@ -1,1 +1,14 @@
-console.log("This is the content script");
+console.log("I've been injected into the page!");
+
+setInterval(() => {
+    const ytRenderer = document.querySelector("ytd-reel-video-renderer");
+    if (ytRenderer) {
+        console.log("Found a video!");
+        const video = ytRenderer.querySelector("video");
+        if (video) {
+            console.log("Found a video element!");
+            video.playbackRate = 10;
+        }
+    }
+}, 200);
+
